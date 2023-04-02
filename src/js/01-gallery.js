1543e27889dc3ce8fbdaa53e26a4f2d404fc50db
Roland Dąbrowski <roli.dabrowski@gmail.com>
2023-04-02 17:05:40 +0200

    added task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+for (const item of galleryItems) {
+  gallery.insertAdjacentHTML(
+    'beforeend',
+    `<li>
+        <a class="gallery__item" href="${item.original}">
+            <img class="gallery__image" src="${item.preview}" alt="Image description" />
+        </a>
+    </li>`
+  );
+}
+
+gallery.addEventListener('click', event => {
+  event.preventDefault();
+});
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionsDelay: 250,
+});
+
+lightbox.on('show.simplelightbox');
