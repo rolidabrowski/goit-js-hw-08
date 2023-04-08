7bde8c7d08268ae33e06c5a6ae860a4d8b3a5f8e
Roland Dąbrowski <roli.dabrowski@gmail.com>
2023-04-08 10:19:03 +0200

    added task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,13 @@
+import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-const saveVideoTime = _.throttle(
+const saveVideoTime = throttle(
   data => localStorage.setItem('videoplayer-current-time', data.seconds),
   1000
 );
 
 player.on('timeupdate', saveVideoTime);
-
 player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
